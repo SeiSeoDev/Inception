9bdd21b4b487c1487e75cdd4e3d1908318f9d824
Damien SANTERRE <dasanter@e2r7p11.clusters.42paris.fr>
2023-01-16 16:06:59 +0100

    almost ddone

---
+++ srcs/mariadb/mariadb.sh
@@ -0,0 +1,18 @@
+touch file 
+chmod 755 file 
+
+cat << stop_file > file
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+USE $MYSQL_DATABASE;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* to '$MYSQL_USER'@'%' identified by '$MYSQL_USER_PASSWORD';
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+stop_file
+
+mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < file
+exec mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
---
+++ srcs/wordpress/wp_script.sh
@@ -0,0 +1,19 @@
+wp core download --allow-root
+
+cp /etc/wp-config.php /var/www/html
+
+chmod -R 755 /var/www/
+
+until mysqladmin -hmysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} ping && \
+         mariadb -hmysql -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "SHOW DATABASES;" | grep ${MYSQL_DATABASE}; do
+    sleep 2
+    echo "waiting to connect..."
+done
+
+
+wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mysql --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root # Configure la DB
+wp core install --url="dasanter.42.fr" --title="Pingu's world" --admin_user="${WORDPRESS_ROOT_LOGIN}"\
+	--admin_password="${WORDPRESS_ROOT_PASSWORD}" --admin_email="asgaulti@student.42.fr" --skip-email --allow-root #Configure le site et l'admin
+wp user create "${WORDPRESS_USER_LOGIN}" --user_pass="${WORDPRESS_USER_PASSWORD}" test@example.com --role=author --allow-root
+
+php-fpm7.3 -F -R
